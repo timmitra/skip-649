b117ab8276d1560df916cfe5cb5a01b630037302
Tim Mitra <tim@it-guy.com>
2025-01-31 12:41:15 -0500

    added Contants from SwiftUI-Elements

--- Sources/Skip649/Helpers/Constants.swift
+++ Sources/Skip649/Helpers/Constants.swift
@@ -5,7 +5,7 @@
 //  Created by Tim Mitra on 2025-01-08.
 //
 
-import Foundation
+import SwiftUI
 
 /*
  for screenshots static let wpURL: String = "https://it-guy.com/wp-new/"
@@ -15,3 +15,63 @@ struct Constants {
     
    static let wpURL: String = "https://hushrealty.ca/concierge/"
 }
+
+struct Constant {
+    
+    struct color {
+        static let tintColor = Color("BrandPrimary")
+        static let foodPrimary = Color("BrandPrimary")
+        static let socialPrimary = Color("SocialPrimary")
+        static let bgDefault = Color("Default")
+        //static let gray = Color("Gray")
+        static let contrast = Color("Contrast")
+        static let travelPrimary = Color("TravelPrimary")
+        static let musicPrimary = Color("MusicPrimary")
+        static let white = Color("white")
+        static let musicButtonColor = Color("MusicButton")
+        static let photographyPrimary = Color("PhotographyPrimary")
+    }
+    
+    struct icon {
+        static let doc = "doc.fill"
+        static let house = "house.fill"
+        static let person = "person.fill"
+        static let magnifyingGlass = "magnifyingglass"
+        static let compose = "square.and.pencil"
+        static let envelope = "envelope.fill"
+        static let heart = "heart"
+        static let share = "square.and.arrow.up"
+        static let map = "map.fill"
+        static let clock = "clock.fill"
+        static let weather = "cloud.sun.fill"
+        static let bed = "bed.double.fill"
+        static let chevronUp = "chevron.up"
+        static let chevronDown = "chevron.down"
+        static let chevronRight = "chevron.right"
+        static let chevronLeft = "chevron.left"
+        static let chevronBack = "chevron.back"
+        static let bookmark = "bookmark"
+        static let creditcard = "creditcard"
+        static let star = "star.fill"
+        static let circle = "circle"
+        static let minus = "minus"
+        static let plus = "plus"
+        static let play = "play.fill"
+        static let pause = "pause.fill"
+        static let playPause = "playpause.fill"
+        static let dots = "ellipsis"
+        static let shuffle = "shuffle"
+        static let forward = "forward.fill"
+        static let backward = "backward.fill"
+        static let repeatIcon = "repeat"
+        static let rectangles = "rectangle.fill.on.rectangle.fill"
+        static let rectangleStack = "rectangle.stack.fill"
+        static let camera = "camera.fill"
+        static let crop = "crop"
+        static let options = "slider.horizontal.3"
+        static let note = "music.note"
+        static let close = "xmark"
+    }
+    
+}
+
